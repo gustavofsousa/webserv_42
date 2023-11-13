034dd0cae95b19cd078ed82dd5d3f950b0f9a2a6
Pedro Arnaldo <pedromadeira201502@outlook.com>
2023-11-13 09:05:51 -0300

    refactor: add ParserRequest class to Request

--- source/http/Request.cpp
+++ source/http/Request.cpp
@@ -1,5 +1,99 @@
-#include "./Request.hpp"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ParserRequest.cpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 15:23:54 by woliveir          #+#    #+#             */
+/*   Updated: 2022/05/09 15:09:49 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-Request::Request() {}
+#include "Request.hpp"
 
-Request::~Request() {}
+Request::Request(std::string buffer)
+{
+	this->parserRequest(buffer);
+}
+
+Request	&Request::operator=(const Request & src)
+{
+	if (this != &src)
+	{
+		this->_method = src._method;
+		this->_location = src._location;
+		this->_requestedInf = src._requestedInf;
+		this->_contentType = src._contentType;
+	}
+	return (*this);
+}
+
+Request::Request(const Request & copy)
+{
+	*this = copy;
+	return ;
+}
+
+Request::~Request(void) {}
+
+const std::string &		Request::getMethod(void) const
+{
+	return (this->_method);
+}
+
+const std::string &		Request::getLocation(void) const
+{
+	return (this->_location);
+}
+
+const std::string &		Request::getRequestedInf(void) const
+{
+	return (this->_requestedInf);
+}
+
+const std::string &		Request::getContentType(void) const
+{
+	return (this->_contentType);
+}
+
+void 	Request::parserRequest(const std::string & request)
+{
+//	std::cout << "start | parserRequest: " << request << std::endl;
+	std::ifstream	ifs;//	temporário até utilização com a string da requisição original
+	std::string		line;//	temporário até utilização com a string da requisição original
+	std::string		fullRequest; //	temporário até utilização com a string da requisição original
+	size_t			pos;
+
+	ifs.open(request.c_str()); //	temporário até utilização com a string da requisição original
+	if (ifs.is_open())//	temporário até utilização com a string da requisição original
+	{
+		while(std::getline(ifs, line))//	temporário até utilização com a string da requisição original
+			fullRequest += (line + '\n');//	temporário até utilização com a string da requisição original
+		ifs.close();//	temporário até utilização com a string da requisição original
+	}
+	pos = fullRequest.find(" HTTP/");//	substituir fullRequest pela string da requisição original
+	if (pos == std::string::npos)
+		std::cout << "Erro na requisição" << std::endl;
+	else
+		this->splitRequest(fullRequest, pos);
+//	std::cout << "end   | parserRequest" << std::endl;
+}
+
+void	Request::splitRequest(std::string & fullRequest, size_t & pos)
+{
+//	std::cout << "start | splitRequest | fullRequest: " << fullRequest << " pos: " << pos << std::endl;
+	std::vector<std::string>	splitHeadRequest;
+	std::vector<std::string>::iterator	i;
+	size_t								j;
+
+	splitHeadRequest = Utils::split(fullRequest.substr(0, pos), " \t\n");
+	i = splitHeadRequest.begin();
+	this->_method = *(i++);
+	j = (*i).rfind("/");
+	this->_location = (*i).substr(0, (j + 1));
+	this->_requestedInf = (*i).substr((j + 1), ((*i).size() - (j + 1)));
+	j = fullRequest.find("Content-Type:");
+	this->_contentType = fullRequest.substr(j, (fullRequest.size() - j));
+//	std::cout << "end   | splitRequest Content-Type: " << j << " contentType: " << this->_contentType << std::endl;
+}
--- source/http/Request.hpp
+++ source/http/Request.hpp
@@ -1,14 +1,43 @@
-#ifndef REQUEST_HPP
-# define REQUEST_HPP
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ParserRequest.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 15:23:54 by woliveir          #+#    #+#             */
+/*   Updated: 2022/05/09 15:09:49 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# include <string.h>
+#pragma	once
 
-class   Request {
-    public:
-        Request();
-        ~Request();
+# include <fstream>
+# include <string>
+# include <iostream> //confirmar se será necessário para a entrega
+# include "./../config/Error.hpp"
+# include "./../config/Utils.hpp"
 
-        //std::string const&  _method;
+class	Request
+{
+	private:
+		std::string	_method;
+		std::string	_location;
+		std::string	_requestedInf;
+		std::string	_contentType;
+		Request(const Request& copy);
+		Request	&operator=(const Request &src);
+
+		void	splitRequest(std::string & fullRequest, size_t & pos);
+
+	public:
+		Request(std::string buffer);
+		~Request(void);
+
+		const std::string &				getMethod(void) const;
+		const std::string &				getLocation(void) const;
+		const std::string &				getRequestedInf(void) const;
+		const std::string &				getContentType(void) const;
+
+		void							parserRequest(const std::string & request);
 };
-
-#endif

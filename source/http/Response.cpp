39e29a75a3e24af4898b249bcfc6b18e7696b001
Pedro Lopes <pedromadeira201502@outlook.com>
2024-01-05 18:08:15 -0300

    feat: implemented class response

--- source/http/Response.cpp
+++ source/http/Response.cpp
@@ -1,5 +1,55 @@
 #include "./Response.hpp"
+#include <fstream>
+#include <iostream>
 
 Response::Response() {}
 
 Response::~Response() {}
+
+std::string Response::toString(int number) {
+    std::stringstream ss;
+    ss << number;
+    std::string str = ss.str();
+    return str;
+}
+
+void Response::setBody(std::string body) {
+    this->_body = body;
+}
+
+void Response::createHTTPHeader(int statusCode, std::string contentType, int contentLength) {
+    std::string header;
+
+    this->_statusCode = statusCode;
+    header.append("HTTP/1.1 ");
+    header.append(toString(statusCode));
+    header.append(" ");
+    header.append("OK");
+    header.append("\r\n");
+    header.append("Content-Type: ");
+    header.append(contentType);
+    header.append("\r\n");
+    header.append("Content-Length: ");
+    header.append(toString(contentLength));
+    header.append("\r\n");
+    header.append("\r\n");
+
+    this->_header = header;
+}
+
+void Response::send() {
+   this->httpMessage = this->_header;
+   this->httpMessage.append(this->_body);
+}
+
+void Response::processFileForHTTPResponse(std::string &file, int statusCode)
+{
+    std::ifstream fileStream(file.c_str());
+    
+    std::stringstream buffer;
+    buffer << fileStream.rdbuf();
+    std::string content = buffer.str();
+
+    this->_body = content;
+    this->createHTTPHeader(statusCode, "text/html", this->_body.size());
+}
--- source/http/Response.hpp
+++ source/http/Response.hpp
@@ -3,14 +3,24 @@
 
 # include <string.h>
 # include <iostream>
+#include <sstream>
 
 class   Response {
     public:
         Response();
         ~Response();
 
-        //std::string const&  _method;
+        void createHTTPHeader(int statusCode, std::string contentType, int contentLength);
+        void send();
+        void setBody(std::string body);
+        std::string toString(int number);
         std::string httpMessage;
+        void        processFileForHTTPResponse(std::string &file, int statusCode);
+    
+    private:
+        std::string _body;
+        std::string _header;
+        int         _statusCode;
 };
 
 #endif
